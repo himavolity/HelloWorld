f4b8e43e8824c79a00f1aad379d8b2626bfac200
himavolity <103455856+himavolity@users.noreply.github.com>
2022-05-19 20:59:58 -0700

    enter

--- force-app/main/default/lwc/contactdetails/contactdetails.js
+++ force-app/main/default/lwc/contactdetails/contactdetails.js
@@ -5,16 +5,36 @@ export default class contactdetails extends NavigationMixin(LightningElement) {
     @api objectApiName;
     redirect = true;
     resetpage = false;
-    @api handlesaveandnext(event) {
-        console.log("I am in contact handlesaveandnext")
-        event.preventDefault();       // stop the form from submitting
-        const fields = event.detail.fields;
-        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    @api
+    handlesaveandnext(event) {
+        if (this.checkError()) {
+            event.preventDefault();
+            const fields = event.detail.fields;
+            this.template.querySelector('lightning-record-edit-form').submit(fields);
+            const even = new ShowToastEvent({
+                title: 'Success!',
+                message: 'Record created!',
+                variant: 'success'
+            });
+            this.dispatchEvent(even);
+        }
+        else {
+            alert('Please fill the required fields')
+        }
+    } checkError() {
+        const areAllValid = true;
+        this.template.querySelectorAll('lightning-input-field').forEach(element => {
+            if (!element.reportValidity()) {
+                element.reportValidity();
+                areAllValid = false;
+            };
+        }); return areAllValid;
     }
+
     handleSucess(event) {
         const RecordNumber = event.detail.id;
         console.log('onsuccess: ', RecordNumber);
-        this.recordId=event.detail.id;
+        this.recordId = event.detail.id;
         const evt = new ShowToastEvent({
             title: "Contact created",
             message: "Record ID: " + event.detail.id,
@@ -22,17 +42,18 @@ export default class contactdetails extends NavigationMixin(LightningElement) {
         });
         this.dispatchEvent(evt);
     }
-    
+
     handlechange() {
 
     }
-    homepage(event){
+    homepage(event) {
         this[NavigationMixin.Navigate]({
             type: 'standard__namedPage',
             attributes: {
                 pageName: 'home',
             },
-        });      }
+        });
+    }
 
 
 }
